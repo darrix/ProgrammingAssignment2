f074d4bf8ef62416d5f44b3d7db6922f7f68a346
darrix <dmhendricks@gmail.com>
2015-02-18 22:12:02 -0500

    Updated caching functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This pair of functions allow the solution to a matrix to be cached
+## and retrieved when needed.  In the case the base matrix is reset, the
+## cached inverse is nulled out
 
-## Write a short comment describing this function
+## this function is the cache mechanism
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(baseMatrix = matrix()){
+  ## create inverse matrix in global environment--initialize it to null
+  inverse <<- NULL
+  set <- function(newMatrix){
+    ## when we reset, capture the matrix and clear the inverse
+    message("clearing cached data")
+    baseMatrix <<- newMatrix
+    inverse <<- NULL
+  }
+  get <- function() baseMatrix  ## return the base matrix
+  setInverse <- function(solvedMatrix) {
+      ## after calculating the inverse matrix, save it
+      message("caching solved matrix")
+      inverse <<- solvedMatrix
+  }
+  getInverse <- function() inverse
+  ## return a list with these parts
+  list(get = get, set = set, setInverse = setInverse, getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## this function refreshes the cached matrix if a new matrix
+## has been created and returns the solved matrix if available
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## retrieve inverse from global env
+  i <- x$getInverse()
+  if (!is.null(i)){
+    ## if inverse is cached, return it
+    message("getting cached data")
+    return(i)
+  }
+  ## else get the base matrix and solve for the inverse
+  baseMatrix <- x$get()
+  i <- solve(baseMatrix)
+  ## cache the result
+  x$setInverse(i)
+  i    ## return the inverse matrix
 }
